5e76c4c4bf07323ae914e74f488e59443d9e618b
jaylee <kerneltrek@gmail.com>
2022-12-26 10:21:06 +0900

    문자열

---
+++ 001_test_cpp/002. 문법 이해/04. 타입과 타입 변환/002.return_double.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+# double 반환
+double a(){
+    return 1.2333;
+}
+
+int main(){
+    double ret = a();
+    cout << ret << "\n";
+    return 0;
+}
---
+++ 001_test_cpp/002. 문법 이해/04. 타입과 타입 변환/003.function header.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double a();
+
+int main(){
+    double ret = a();
+    cout << ret << "\n";
+    return 0;
+}
+double a(){
+    return 1.2333;
+}
